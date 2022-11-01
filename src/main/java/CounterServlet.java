f19eb69349af3b419c9f17cd507d2926bf5f5c31
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-11-01 13:58:29 -0500

    servlets exercise counter

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -4,19 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    int count = 0;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        count += 1;
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        String name = req.getParameter("name");
 
-        if (name == null) {
-            out.println("<h1>Hello, World!</h1>");
-        } else {
-            out.println("<h1>Hello, " + name + "</h1>");
+        if (req.getParameter("reset") != null && req.getParameter("reset").equals("true")){
+            count = 0;
+            res.sendRedirect("/count");
         }
+
+        out.println("Count: " + count);
     }
 }
