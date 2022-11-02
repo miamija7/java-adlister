8739c364afed6105d8dd8319c866811b70bcf806
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-11-02 14:36:23 -0500

    national park servlet - lecture

--- src/main/java/NationalParkServlet.java
+++ src/main/java/NationalParkServlet.java
@@ -8,7 +8,18 @@ import java.io.IOException;
 @WebServlet(name = "NationalParkServlet", urlPatterns = "/nps")
 public class NationalParkServlet extends HttpServlet{
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        req.getRequestDispatcher("/nps.jsp").forward(req, res);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        // System.out.println(req.getParameter("np"));
+
+        // Add a req attribute of nationalParks accessed by "nationalParkList"
+        // String[] nationalParks = {"Grand Canyon National Park", "Yosemite National Park", "Denali National Park", "Arches National Park", "Glacier National Park"};
+        // request.setAttribute("nationalParkList", nationalParks);
+
+        // request.getRequestDispatcher("/nps.jsp").forward(request, response);
+
+        // Java Object?
+        Dog d1 = new Dog("Sih-tzu", "Rondo", 9);
+        request.setAttribute("myDog", d1);
+        request.getRequestDispatcher("/nps.jsp").forward(request, response);
     }
 }
--- src/main/webapp/nps.jsp
+++ src/main/webapp/nps.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -6,6 +7,14 @@
 <body>
 <h1>National Parks</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur quam, unde! Culpa dolores et fuga hic ipsa magnam maxime necessitatibus omnis optio perspiciatis, possimus reiciendis repudiandae velit? Ipsa, praesentium quisquam?</p>
+<%--<ul>--%>
+<%--    <c:forEach items="${nationalParkList}" var="park">--%>
+<%--        <li>${park}</li>--%>
+<%--    </c:forEach>--%>
+<%--</ul>--%>
 
+<h4>${myDog.name}</h4>
+<p>Breed: ${myDog.breed}</p>
+<p>Age: ${myDog.age}</p>
 </body>
 </html>
