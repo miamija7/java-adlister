52254b41f6bf68ebe6fcf8fe5d47b190ad3db2f6
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-11-07 13:07:38 -0600

    ads exerxise

---
+++ src/main/java/AdsServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class AdsServlet {
+    @WebServlet("/products")
+    public class ShowProductsServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+            // Use the factory to get the dao object
+            Ads adsDao = DaoFactory.getAdsDao();
+            // Use a method on the dao to get all the products
+            List<Ad> ads = adsDao.all();
+            // Pass the data to the jsp
+            req.setAttribute("ads", ads);
+            req.getRequestDispatcher("/ads/index.jsp").forward(req, res);
+        }
+    }
+}
